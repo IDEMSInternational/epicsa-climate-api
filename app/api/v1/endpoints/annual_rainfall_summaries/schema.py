c260d2d448e442501ff85ab0ac439d62bba6d396
Chris <chris.m.clarke@live.co.uk>
2023-03-21 14:26:35 +0000

    feat: add default values for annual summaries

--- app/api/v1/endpoints/annual_rainfall_summaries/schema.py
+++ app/api/v1/endpoints/annual_rainfall_summaries/schema.py
@@ -1,7 +1,17 @@
-from typing import Optional, List
+from typing import Literal, List, Optional
 from pydantic import BaseModel
 
+summary_name = Literal[
+    "seasonal_rainfall",
+    "seasonal_raindays",
+    "start_rains",
+    "end_season",
+    "length_season"
+    ]
+
+country_name = Literal["zm","mw"]   
+
 class AnnualRainfallSummariesParameters(BaseModel):
-    country: str
-    station_id: str
-    summaries: List[str] = None
+    country: country_name = "zm"
+    station_id: str = "16"
+    summaries: Optional[List[summary_name]] = None
