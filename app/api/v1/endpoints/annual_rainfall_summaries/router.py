ea5696a92ed24759cbc844d917b403f6bf99cf84
Stephen Lloyd <stephen.lloyd03@gmail.com>
2023-03-21 13:13:22 +0100

    updated to use latest version of epicsa-python (not working yet)

--- app/api/v1/endpoints/annual_rainfall_summaries/router.py
+++ app/api/v1/endpoints/annual_rainfall_summaries/router.py
@@ -3,8 +3,9 @@ from typing import Any, List
 from fastapi import APIRouter, Depends, HTTPException
 
 from .schema import AnnualRainfallSummariesParameters
-#todo
-#from app.epicsa_python.main import annual_rainfall_summaries
+
+# todo
+# from app.epicsa_python.main import annual_rainfall_summaries
 from epicsa_python.epicsa import annual_rainfall_summaries
 
 from app.utils.response import get_dataframe_response
@@ -13,11 +14,9 @@ router = APIRouter()
 
 
 @router.post("/")
-def get_annual_rainfall_summaries(
-    params: AnnualRainfallSummariesParameters
-):
+def get_annual_rainfall_summaries(params: AnnualRainfallSummariesParameters):
     """
     TODO.
     """
-    df = annual_rainfall_summaries(params.station_id, params.summaries)
+    df = annual_rainfall_summaries(params.country, params.station_id, params.summaries)
     return get_dataframe_response(df)
--- app/api/v1/endpoints/annual_rainfall_summaries/schema.py
+++ app/api/v1/endpoints/annual_rainfall_summaries/schema.py
@@ -2,5 +2,6 @@ from typing import Optional, List
 from pydantic import BaseModel
 
 class AnnualRainfallSummariesParameters(BaseModel):
+    country: str
     station_id: str
     summaries: List[str] = None
