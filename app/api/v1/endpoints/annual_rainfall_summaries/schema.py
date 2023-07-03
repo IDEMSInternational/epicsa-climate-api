87a40f6c1c17adf1815d3d4aa0856c87efee87af
Stephen Lloyd <stephen.lloyd03@gmail.com>
2023-07-03 12:23:00 +0200

    Correct default parameter values

--- app/api/v1/endpoints/annual_rainfall_summaries/schema.py
+++ app/api/v1/endpoints/annual_rainfall_summaries/schema.py
@@ -2,14 +2,24 @@ from typing import Literal, List, Optional
 from pydantic import BaseModel
 
 summary_name_annual_rainfall = Literal[
-    "annual_rain", "start_rains", "end_rains", "seasonal_length", "end_season"
+    "annual_rain",
+    "start_rains",
+    "end_rains",
+    "end_season",
+    "seasonal_rain",
+    "seasonal_length",
 ]
-summary_name = Literal["mean_tmin", "mean_tmax"]
-
 country_name = Literal["zm", "mw"]
 
 
 class AnnualRainfallSummariesParameters(BaseModel):
     country: country_name = "zm"
     station_id: str = "01122"
-    summaries: Optional[List[summary_name_annual_rainfall]] = []
+    summaries: Optional[List[summary_name_annual_rainfall]] = [
+        "annual_rain",
+        "start_rains",
+        "end_rains",
+        "end_season",
+        "seasonal_rain",
+        "seasonal_length",
+    ]
