7a1da4d60a3fe8c140f7805740593603698f3469
chrismclarke <chris.m.clarke@live.co.uk>
2024-02-09 12:05:45 -0800

    chore: add country_code and language_code types

--- app/definitions.py
+++ app/definitions.py
@@ -1,4 +1,10 @@
 from typing import Literal
 
 
-country_name = Literal["zm", "mw"]
+# ISO 3166 country code (https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes)
+# alpha-2 used where available
+country_code = Literal['zm','mw']
+
+# ISO 639 language code (https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes)
+# set 1 used where available
+language_code = Literal['en','ny']
