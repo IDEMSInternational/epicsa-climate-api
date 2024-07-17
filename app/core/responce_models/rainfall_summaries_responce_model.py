7e3aee5bdf3905f1dfd58f8afaa0f2c51c4e1761
Chris Marsh <chris.marsh@idems.international>
2024-07-17 13:48:42 +0100

    fixes to previous PR and updated version

--- app/core/responce_models/rainfall_summaries_responce_model.py
+++ app/core/responce_models/rainfall_summaries_responce_model.py
@@ -24,8 +24,7 @@ class AnnualRainfallSummariesdata(BaseModel):
     seasonal_rain:Optional[int] 
     season_length: Optional[float]   
     end_season_doy:Optional[int] 
-    end_season_date:Optional[str] | object #rpy2 has issue with no recognising null strings
-    season_length: Optional[float]     
+    end_season_date:Optional[str] | object #rpy2 has issue with no recognising null strings  
      
 
 class AnnualRainfallSummariesResponce(BaseModel):
