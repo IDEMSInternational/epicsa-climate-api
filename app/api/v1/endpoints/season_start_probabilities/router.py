c30cbf5d34bd1d8e9fca394a2b8937cbbfb8687e
Chris Marsh <chris.marsh@idems.international>
2025-07-17 10:18:17 +0100

    removed unused import

--- app/api/v1/endpoints/season_start_probabilities/router.py
+++ app/api/v1/endpoints/season_start_probabilities/router.py
@@ -2,7 +2,7 @@ from typing import OrderedDict
 
 from app.epicsawrap_link import season_start_probabilities
 from fastapi import APIRouter, Depends
-from app.api.v1.endpoints.epicsa_data import RunEpicsaFunctionType, get_run_epicsa_function, handle_epicsa_request, run_epicsa_function_and_get_dataframe
+from app.api.v1.endpoints.epicsa_data import RunEpicsaFunctionType, get_run_epicsa_function, handle_epicsa_request
 from app.core.responce_models.season_start_probabilities import SeasonStartProbabilitiesResponce
 
 from .schema import (
